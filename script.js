187dc1d1689838eec8a7d54bd3c3fba2d8c88b33
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-24 11:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const price=document.querySelectorAll(".price");
+	let total=0;
+	price.forEach((price)=>{
+		total+=parseFloat(price.textContent);
+	})
+	const totalRow = document.createElement("tr");
+  const totalItemCell = document.createElement("td");
+  totalItemCell.textContent = "Total Price";
+  totalItemCell.setAttribute("colspan", "2");
+  totalRow.appendChild(totalItemCell);
+
+  const totalPriceCell = document.createElement("td");
+  totalPriceCell.textContent = total;
+  totalRow.appendChild(totalPriceCell);
+
+  document.querySelector("table").appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
